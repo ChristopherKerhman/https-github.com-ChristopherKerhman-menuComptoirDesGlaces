85a885236f0045ff8a3b5d840483660300a777f0
Calmes <77333391+ChristopherKerhman@users.noreply.github.com>
2021-08-06 00:17:19 +0200

    Engnime du push "résolus", ça marche...

--- rubriques/uneBoule.php
+++ rubriques/uneBoule.php
@@ -26,6 +26,10 @@
     <li class="liCoupe">prix {{prix}} €</li>
   </ul></h4>
   <section id="selectionBoule">
+    <ul>
+      <li class="liSansStyle"><h4>Les coupes que vous avez enregistré :</h4></li>
+      <li class="liSansStyle" v-for="element in panier" v-bind:key="element">{{element}} €</li>
+    </ul>
     <div v-if="coupe.length < 1" class="flexRows">
       <div>
         <h3>Crème glacée</h3>
@@ -55,18 +59,22 @@
       const boule1 = Vue.createApp({
         data () {
           return {
-            selectionCreme: '',
-            selectionSorbet: '',
-            selectionSup: '',
             coupe: [],
             prix: 0,
-            indexation: [],
+            panier: [],
             selection: '',
             creme: <?php echo $creme; ?>,
             sorbet: <?php echo $sorbet; ?>,
             sup: <?php echo $sup; ?>
 
           }
+        },
+        mounted () {
+          let sac
+          for (var i = 0; i < localStorage.length; i++) {
+           sac = localStorage.getItem(localStorage.key(i))
+           this.panier.push(sac)
+        }
         },
         methods: {
           creationCreme (nom) {
@@ -80,14 +88,13 @@
             this.prix +=1
           },
           rec () {
-            const KEY = Math.floor(Math.random() * (6000 - 1 + 1 )) + 1
+            const KEY = Math.floor(Math.random() * (10000000 - 1 + 1 )) + 1
             this.coupe.push(this.prix)
-            sessionStorage.setItem(KEY, this.coupe)
+            localStorage.setItem(KEY, this.coupe)
             location.reload()
-
           },
           del () {
-            sessionStorage.clear()
+            localStorage.clear()
             location.reload()
           }
         }
